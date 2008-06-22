ac02671dbcdf35b534decabd72850bfbcd31f02d
kj187 <kj187@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-06-22 21:21:51 +0000

    Include javascript for backend hotkeys
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/hotkeys/trunk@10192 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.additionalBackendItems.hook.php
+++ class.additionalBackendItems.hook.php
@@ -3,32 +3,7 @@ if (!defined ("TYPO3_MODE")) 	die ("Access denied.");
 
 $extRelPath = t3lib_extMgm::extRelPath('hotkeys');
 
-
-// jquery 
-#$GLOBALS['TYPO3backend']->addJavascriptFile($extRelPath.'res/jquery/jquery-1.2.6.js');
-#$GLOBALS['TYPO3backend']->addJavascriptFile($extRelPath.'res/jquery/jquery.hotkeys.js');
-#$GLOBALS['TYPO3backend']->addJavascriptFile($extRelPath.'res/jquery/hotkeys.js');
-
-
-// Acunote
-#$GLOBALS['TYPO3backend']->addJavascriptFile($extRelPath.'res/acunote/acunote-shortcuts.js');
-#$GLOBALS['TYPO3backend']->addJavascriptFile($extRelPath.'res/acunote/typo3-shortcuts.js');
-#$GLOBALS['TBE_TEMPLATE']->bodyTagAdditions = 'onload="shortcutListener.init();"';
-
-
-// OpenJS
-#$GLOBALS['TYPO3backend']->addJavascriptFile($extRelPath.'res/openjs/openjs-shortcuts.js');
-#$GLOBALS['TYPO3backend']->addJavascriptFile($extRelPath.'res/openjs/typo3-shortcuts.js');
-#$GLOBALS['TBE_TEMPLATE']->bodyTagAdditions = 'onload="typo3ShortcutInit();"';
-
-
-// prototype 
-#$GLOBALS['TYPO3backend']->addJavascriptFile($extRelPath.'res/prototype/livepipe/src/livepipe.js');
-#$GLOBALS['TYPO3backend']->addJavascriptFile($extRelPath.'res/prototype/livepipe/src/hotkey.js');
-#$GLOBALS['TYPO3backend']->addJavascriptFile($extRelPath.'res/prototype/typo3.hotkey.js');
-
-
-// own 
-$GLOBALS['TYPO3backend']->addJavascriptFile($extRelPath.'res/own/typo3-hotkeys.js');
+// Include javascript for backend hotkeys 
+$GLOBALS['TYPO3backend']->addJavascriptFile($extRelPath.'res/typo3_hotkeys.js');
 
 ?>
